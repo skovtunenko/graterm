6523cdbacb787f420f6adbd168e09eaeeacc4bf5
skovtunenko <skovtunenko@exadel.com>
2022-07-02 20:12:02 +0300

    Added examples for `Stopper.Wait()` and `Stopper.ExampleStopper_AddShutdownHook()` methods

---
+++ example_test.go
@@ -0,0 +1,54 @@
+package graterm_test
+
+import (
+	"context"
+	"github.com/skovtunenko/graterm"
+	"log"
+	"time"
+)
+
+func ExampleStopper_Wait() {
+	stopper, appCtx := graterm.NewWithDefaultSignals(context.Background(), log.Default())
+
+	// register hooks...
+
+	// Wire other components ...
+
+	if err := stopper.Wait(appCtx, 40*time.Second); err != nil {
+		log.Printf("graceful termination period is timed out: %+v", err)
+	}
+}
+
+func ExampleStopper_AddShutdownHook() {
+	stopper, appCtx := graterm.NewWithDefaultSignals(context.Background(), log.Default())
+	// Register some hooks:
+	{
+		stopper.AddShutdownHook(1, "HOOK#1", 1*time.Second, func(ctx context.Context) {
+			log.Println("terminating HOOK#1...")
+			defer log.Println("...HOOK#1 terminated")
+		})
+
+		stopper.AddShutdownHook(1, "HOOK#2", 1*time.Second, func(ctx context.Context) {
+			log.Println("terminating HOOK#2...")
+			defer log.Println("...HOOK#2 terminated")
+		})
+
+		stopper.AddShutdownHook(2, "HOOK#3", 1*time.Second, func(ctx context.Context) {
+			log.Println("terminating HOOK#3...")
+			defer log.Println("...HOOK#3 terminated")
+
+			sleepTime := 3 * time.Second
+			t := time.NewTimer(sleepTime)
+			select {
+			case <-t.C:
+				log.Printf("HOOK#3 sleep time %v is over\n", sleepTime)
+			case <-ctx.Done():
+				log.Printf("HOOK#3 Context is Done because of: %+v\n", ctx.Err())
+			}
+		})
+
+		if err := stopper.Wait(appCtx, 20*time.Second); err != nil {
+			log.Printf("graceful termination period is timed out: %+v", err)
+		}
+	}
+}
