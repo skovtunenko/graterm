012258992812dc9295b9d616e53ea5f2809e4d96
Sergio Kovtunenko <skovtunenko@users.noreply.github.com>
2022-07-02 15:44:22 +0300

    Added constructor to create Stopper, added integration with os signals (#6)
    * Added `withSignals()`
    
    * Added `NewWithSignals()` constructor for Stopper
    
    * Added `NewWithDefaultSignals()` constructor for Stopper

--- term.go
+++ term.go
@@ -2,9 +2,15 @@ package graterm
 
 import (
 	"context"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 )
+
+// defaultSignals is a default set of signals to handle.
+var defaultSignals = []os.Signal{syscall.SIGINT, syscall.SIGTERM}
 
 // TerminationOrder is an application components termination order.
 //
@@ -27,4 +33,50 @@ type Stopper struct {
 	cancelFunc context.CancelFunc // todo check later on if this needed?
 
 	log Logger
+}
+
+// NewWithDefaultSignals creates a new instance of application component stopper.
+// invokes withSignals with syscall.SIGINT and syscall.SIGTERM as default signals.
+//
+// Note: this method will start internal monitoring goroutine.
+func NewWithDefaultSignals(appCtx context.Context, log Logger) (*Stopper, context.Context) {
+	return NewWithSignals(appCtx, log, defaultSignals...)
+}
+
+// NewWithSignals creates a new instance of application component stopper.
+//
+// Note: this method will start internal monitoring goroutine.
+func NewWithSignals(appCtx context.Context, log Logger, sig ...os.Signal) (*Stopper, context.Context) {
+	ctx, cancel := withSignals(appCtx, sig...)
+	return &Stopper{
+		termComponentsMx: &sync.Mutex{},
+		termComponents:   make(map[TerminationOrder][]terminationFunc),
+		wg:               &sync.WaitGroup{},
+		cancelFunc:       cancel,
+		log:              log,
+	}, ctx
+}
+
+// withSignals return a copy of the parent context that will be canceled by signal.
+// If no signals are provided, any incoming signal will cause cancel.
+// Otherwise, just the provided signals will.
+//
+// Note: this method will start internal monitoring goroutine.
+func withSignals(ctx context.Context, sig ...os.Signal) (context.Context, context.CancelFunc) {
+	ctx, cancel := context.WithCancel(ctx)
+
+	chSignals := make(chan os.Signal, 1)
+	signal.Notify(chSignals, sig...)
+
+	// function invoke cancel once a signal arrived OR parent context is done:
+	go func() {
+		defer cancel()
+
+		select {
+		case <-chSignals:
+		case <-ctx.Done():
+		}
+	}()
+
+	return ctx, cancel
 }
