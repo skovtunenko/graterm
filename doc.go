bcaf16e268cd5e0f06a02758c83f1a22e85aa26e
Sergio Kovtunenko <skovtunenko@users.noreply.github.com>
2022-07-03 15:29:00 +0300

    Provided initial basic package documentation (#24)

---
+++ doc.go
@@ -0,0 +1,3 @@
+// Package graterm provides capabilities to create a Stopper instance, register prioritized termination Hooks,
+// and block application execution until one of the registered os.Signal events occurs.
+package graterm
