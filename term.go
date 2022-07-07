912afecfedd009d00a36bb63d68c20740c293959
Sergio Kovtunenko <skovtunenko@users.noreply.github.com>
2022-07-07 13:23:41 +0300

    Removed unused `Server` abstraction interface (#40)

--- term.go
+++ term.go
@@ -33,13 +33,6 @@ type Terminator struct {
 	log Logger
 }
 
-// Server is a generic Server. Well-known implementation is a built-in http.Server.
-// Interface was introduced for testing purposes mainly.
-type Server interface {
-	ListenAndServe() error
-	Shutdown(ctx context.Context) error
-}
-
 // NewWithSignals creates a new instance of component Terminator.
 //
 // Example of useful signals might be: syscall.SIGINT, syscall.SIGTERM.
